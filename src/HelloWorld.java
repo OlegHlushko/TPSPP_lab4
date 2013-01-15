eae5390c3a422d7d553f6d954dab9695c71158cc
unknown <oleghlushko@gmail.com>
2013-01-15 03:19:06 +0200

    Signed-off-by: unknown <oleghlushko@gmail.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,7 +6,8 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hellow World!");
+		System.out.println("Hellow World! from Oleg Hlushko");
+		
 	}
 
 }
